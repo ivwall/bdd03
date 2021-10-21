4b1c6595f453d68af0708cf8e735ac471bcef92d
ivwall <ivwall@tgndomains.com>
2021-10-21 14:16:29 -0700

    filled out to steps of the scario

--- webkernel/src/test/java/webkernel/StepDefinitions.java
+++ webkernel/src/test/java/webkernel/StepDefinitions.java
@@ -16,7 +16,6 @@ public class StepDefinitions {
 
     @Given("^Open the Firefox and launch the application$")				
     public void open_the_Firefox_and_launch_the_application() throws Throwable	{		
-        System.out.println("This Step open the Firefox and launch the application.");					
         System.setProperty("webdriver.chrome.driver", "D://2021//1021-webcu//bdd03//webkernel//target//dependency//chromedriver.exe");					
         driver= new ChromeDriver();					
         driver.manage().window().maximize();			
@@ -25,12 +24,13 @@ public class StepDefinitions {
 
     @When("^Enter the Username and Password$")					
     public void enter_the_Username_and_Password() throws Throwable 	{		
-       System.out.println("This step enter the Username and Password on the login page.");					
-    }		
+       driver.findElement(By.name("uid")).sendKeys("username12");							
+       driver.findElement(By.name("password")).sendKeys("password12");							
+   }		
 
     @Then("^Reset the credential$")					
     public void Reset_the_credential() throws Throwable {    		
-        System.out.println("This step click on the Reset button.");					
+        driver.findElement(By.name("btnReset")).click();					
     }		
 
 }
