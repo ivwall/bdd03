fc1ae542f7a39dead1e3142f84581515f446cb8a
ivwall <ivwall@tgndomains.com>
2021-10-21 14:31:52 -0700

    enter cheese in search field, hit enter

--- webkernel/src/test/java/webkernel/StepDefinitions.java
+++ webkernel/src/test/java/webkernel/StepDefinitions.java
@@ -1,6 +1,7 @@
 package webkernel;
 
 import org.openqa.selenium.By;		
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;		
 import org.openqa.selenium.chrome.ChromeDriver;		
 
@@ -41,17 +42,14 @@ public class StepDefinitions {
         driver= new ChromeDriver();					
         driver.manage().window().maximize();			
         driver.get("https://google.com/ncr");	        
-        //System.setProperty("webdriver.chrome.driver", "D://2021//1021-webcu//bdd03//webkernel//target//dependency//chromedriver.exe");					
-        //driver= new ChromeDriver();					
-        //driver.manage().window().maximize();			
-        //driver.get("http://demo.guru99.com/v4");	        
     }		
 
     @When("^Enter cheese into search$")					
     public void enter_cheese_into_search() throws Throwable 	{		
         System.out.println("Enter chees into search");
-       //driver.findElement(By.name("uid")).sendKeys("username12");							
-       //driver.findElement(By.name("password")).sendKeys("password12");							
+        driver.findElement(By.name("q")).sendKeys("cheese" + Keys.ENTER);
+        //driver.findElement(By.name("uid")).sendKeys("username12");							
+        //driver.findElement(By.name("password")).sendKeys("password12");							
    }		
 
     @Then("^Hit enter$")					
