031d5d6c107bfabb2a5b3700b625d40166f18f1c
ivwall <ivwall@tgndomains.com>
2021-10-21 14:25:58 -0700

    added a scenario find the cheese, put printlns as first step

--- webkernel/src/test/java/webkernel/StepDefinitions.java
+++ webkernel/src/test/java/webkernel/StepDefinitions.java
@@ -33,4 +33,31 @@ public class StepDefinitions {
         driver.findElement(By.name("btnReset")).click();					
     }		
 
+    //-------------  A new Scenario -------------------------------------------------------------
+
+    @Given("^Open Chrome set to google$")				
+    public void open_chrome_set_to_google() throws Throwable	{		
+        System.out.println("Open Chrome set to google");
+        //System.setProperty("webdriver.chrome.driver", "D://2021//1021-webcu//bdd03//webkernel//target//dependency//chromedriver.exe");					
+        //driver= new ChromeDriver();					
+        //driver.manage().window().maximize();			
+        //driver.get("http://demo.guru99.com/v4");	        
+    }		
+
+    @When("^Enter cheese into search$")					
+    public void enter_cheese_into_search() throws Throwable 	{		
+        System.out.println("Enter chees into search");
+       //driver.findElement(By.name("uid")).sendKeys("username12");							
+       //driver.findElement(By.name("password")).sendKeys("password12");							
+   }		
+
+    @Then("^Hit enter$")					
+    public void hit_enter() throws Throwable {    		
+        System.out.println("Hit enter");
+        //driver.findElement(By.name("btnReset")).click();					
+    }		
+
+
+
+
 }
--- webkernel/target/test-classes/webkernel/bdd03.feature
+++ webkernel/target/test-classes/webkernel/bdd03.feature
@@ -4,3 +4,8 @@ Feature: Reset functionality on login page of Application
         Given Open the Firefox and launch the application			
         When Enter the Username and Password			
         Then Reset the credential
+
+    Scenario: Looking for the Cheese
+        Given Open Chrome set to google
+        When Enter cheese into search
+        Then Hit enter
